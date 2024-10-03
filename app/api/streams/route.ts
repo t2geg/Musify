38b43b71847e2d8e8aeacb4893ed0d70df5d5523
t2geg <ashishgupta59684@gmail.com>
2024-10-03 22:35:47 +0530

    added: fetch details of yt vid

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -1,6 +1,9 @@
 import { prismaClient } from "@/app/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
+//@ts-ignore
+import youtubesearchapi from "youtube-search-api";
+
 const YT_REGEX = new RegExp("^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\/(watch\?v=|embed\/|v\/|.+\?v=)?([a-zA-Z0-9_-]{11})(\S*)?$");
 
 const CreateStreamSchema = z.object({
@@ -23,12 +26,20 @@ export async function POST(req: NextRequest) {
 
         const extractedId = data.url.split("?v=")[1];
 
+        const res = await youtubesearchapi.GetVideoDetails(extractedId);
+
+        const thumbnails = res.thumbnail.thumbnails;
+        thumbnails.sort((a: { width: number }, b: { width: number }) => a.width < b.width ? -1 : 1);
+
         const stream = await prismaClient.stream.create({
             data: {
                 userId: data.creatorId,
                 url: data.url,
                 extractedId,
-                type: "Youtube"
+                type: "Youtube",
+                title: res.title ?? "can't find video",
+                bigImg: thumbnails[thumbnails.length - 1].url ?? "https://www.istockphoto.com/photos/music",
+                smallImg: thumbnails.length > 1 ? thumbnails[thumbnails.length - 2].url : thumbnails[thumbnails.length - 1].url ?? "https://www.istockphoto.com/photos/music"
             }
 
         })
